d81f899e4d925e29248796d270249da73e1b225e
Leozinho11 <leoferraribarreto@hotmail.com>
2024-03-18 07:07:31 -0300

    Remove unused imports

--- project-harbor-api/src/main/java/com/harbor/projectharborapi/agendamento/IAgendamento.java
+++ project-harbor-api/src/main/java/com/harbor/projectharborapi/agendamento/IAgendamento.java
@@ -3,10 +3,7 @@ package com.harbor.projectharborapi.agendamento;
 import com.harbor.projectharborapi.cliente.Cliente;
 import com.harbor.projectharborapi.pedido.Pedido;
 import com.harbor.projectharborapi.prestadores.abstrato.PrestadorDeServico;
-import com.harbor.projectharborapi.servicos.Servico;
 
-import java.time.LocalDateTime;
-import java.util.List;
 
 public interface IAgendamento {
     Pedido agendarPedido(PrestadorDeServico p, Cliente c, Pedido novoPedido);

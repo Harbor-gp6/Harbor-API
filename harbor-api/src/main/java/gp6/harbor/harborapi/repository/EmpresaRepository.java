5498d89a9647d37df41c311b5b3d402060df5ee1
Vitor Ramos <vitoramosc@gmail.com>
2024-04-05 19:27:34 -0300

    Repositorio da empresa criada.

--- harbor-api/src/main/java/gp6/harbor/harborapi/repository/EmpresaRepository.java
+++ harbor-api/src/main/java/gp6/harbor/harborapi/repository/EmpresaRepository.java
@@ -1,10 +1,15 @@
 package gp6.harbor.harborapi.repository;
 
 import gp6.harbor.harborapi.entity.Cliente;
+import gp6.harbor.harborapi.entity.Empresa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface EmpresaRepository extends JpaRepository<Cliente, Integer> {
+public interface EmpresaRepository extends JpaRepository<Empresa, Integer> {
+
+    List<Empresa> findByRazaoSocialContainingIgnoreCase(String razaoSocial);
+
+    boolean existsByCnpj(String cnpj);
 
 }

757e02068a4cb1cd635914e5e29176d41c7897bd
Vitor Ramos <vitoramosc@gmail.com>
2024-04-05 19:28:10 -0300

    Entidade empresa criada.

--- harbor-api/src/main/java/gp6/harbor/harborapi/entity/Empresa.java
+++ harbor-api/src/main/java/gp6/harbor/harborapi/entity/Empresa.java
@@ -1,18 +1,79 @@
 package gp6.harbor.harborapi.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.br.CNPJ;
+
+
+import java.time.LocalDate;
 
 @Entity
 public class Empresa {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @NotBlank(message = "A razão social não pode estar em branco")
     @Size(min = 2, max = 300, message = "A razão social deve ter entre 2 e 300 caracteres")
     private String razaoSocial;
+    @Size(max = 255)
+    private String nomeFantasia;
+    @CNPJ
+    private String cnpj;
+    @OneToOne
+    @JoinColumn(name = "id_endereco")
+    private Endereco endereco;
+    private LocalDate dataCriacao;
+
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
 }

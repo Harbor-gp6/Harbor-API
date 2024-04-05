12ae4f5f9369eef4659e1d2d725e9724339657ad
Vitor Ramos <vitoramosc@gmail.com>
2024-04-05 19:28:55 -0300

    Entidade cliente criado.

--- harbor-api/src/main/java/gp6/harbor/harborapi/entity/Cliente.java
+++ harbor-api/src/main/java/gp6/harbor/harborapi/entity/Cliente.java
@@ -8,25 +8,26 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDate;
 
 @Entity
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @NotBlank(message = "O nome não pode estar em branco")
-    @Size(min = 2, max = 100, message = "O nome deve ter entre 2 e 100 caracteres")
+
     private String nome;
-    @NotBlank(message = "O sobrenome não pode estar em branco")
-    @Size(min = 2, max = 100, message = "O sobrenome deve ter entre 2 e 100 caracteres")
+
     private String sobrenome;
-    @NotBlank
-    @Pattern(regexp = "\\d{10,11}", message = "O telefone deve conter apenas números e deve ter entre 10 ou 11 dígitos")
+
     private String telefone;
-    @CPF
-    @NotBlank
+
     private String cpf;
+    @CreationTimestamp
+    private LocalDate dataCriacao;
 
     public Integer getId() {
         return id;
@@ -66,5 +67,13 @@ public class Cliente {
 
     public void setCpf(String cpf) {
         this.cpf = cpf;
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
     }
 }

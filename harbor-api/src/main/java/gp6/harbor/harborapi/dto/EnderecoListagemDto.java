7aed291b0df6e45f65884bef46b055a97b09f4e5
Vitor Ramos <vitoramosc@gmail.com>
2024-04-05 19:26:35 -0300

    Dto de listagem do endereço criada.

---
+++ harbor-api/src/main/java/gp6/harbor/harborapi/dto/EnderecoListagemDto.java
@@ -0,0 +1,49 @@
+package gp6.harbor.harborapi.dto;
+
+public class EnderecoListagemDto {
+    private Integer id;
+    private String logradouro;
+    private String cidade;
+    private String estado;
+    private String cep;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+}

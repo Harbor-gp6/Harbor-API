f43277a234b41c251a4b41d5975e6530d33b731c
Vitor Ramos <vitoramosc@gmail.com>
2024-04-05 19:26:03 -0300

    Mapper do Endereço criado.

---
+++ harbor-api/src/main/java/gp6/harbor/harborapi/dto/EnderecoMapper.java
@@ -0,0 +1,44 @@
+package gp6.harbor.harborapi.dto;
+
+import gp6.harbor.harborapi.entity.Cliente;
+import gp6.harbor.harborapi.entity.Endereco;
+
+import java.util.List;
+
+public class EnderecoMapper {
+
+    public static Endereco toEntity(EnderecoCriacaoDto EnderecoDto){
+        if (EnderecoDto == null){
+            return null;
+        }
+
+        Endereco endereco = new Endereco();
+
+        endereco.setBairro(EnderecoDto.getBairro());
+        endereco.setLogradouro(EnderecoDto.getLogradouro());
+        endereco.setCidade(EnderecoDto.getCidade());
+        endereco.setEstado(EnderecoDto.getEstado());
+        endereco.setNumero(EnderecoDto.getNumero());
+        endereco.setCep(EnderecoDto.getCep());
+
+        return endereco;
+    }
+
+    public static EnderecoListagemDto toDto(Endereco entity){
+        if (entity == null) return null;
+
+        EnderecoListagemDto listagemDto = new EnderecoListagemDto();
+        listagemDto.setId(entity.getId());
+        listagemDto.setLogradouro(entity.getLogradouro());
+        listagemDto.setCidade(entity.getCidade());
+        listagemDto.setEstado(entity.getEstado());
+        listagemDto.setCep(entity.getCep());
+
+        return listagemDto;
+
+    }
+
+    public static List<EnderecoListagemDto> toDto(List<Endereco> entities){
+        return entities.stream().map(EnderecoMapper::toDto).toList();
+    }
+}

f3d24f9573c7e5401c6d8d32634b916bf6687021
Vitor Ramos <vitoramosc@gmail.com>
2024-04-05 19:25:54 -0300

    Repositorio do Endereço criado.

---
+++ harbor-api/src/main/java/gp6/harbor/harborapi/repository/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package gp6.harbor.harborapi.repository;
+
+import gp6.harbor.harborapi.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+    Endereco findByCepEquals(String cep);
+
+}

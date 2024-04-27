214d11c0523911d34748435ef05b364952f595c7
Vitor Ramos <vitoramosc@gmail.com>
2024-04-26 22:17:26 -0300

    correção de bug na servicoRepository

--- harbor-api/src/main/java/gp6/harbor/harborapi/domain/servico/repository/ServicoRepository.java
+++ harbor-api/src/main/java/gp6/harbor/harborapi/domain/servico/repository/ServicoRepository.java
@@ -3,5 +3,6 @@ package gp6.harbor.harborapi.domain.servico.repository;
 import gp6.harbor.harborapi.domain.servico.Servico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ServicoRepository extends JpaRepository<Integer, Servico> {
+public interface ServicoRepository extends JpaRepository<Servico, Integer> {
+
 }

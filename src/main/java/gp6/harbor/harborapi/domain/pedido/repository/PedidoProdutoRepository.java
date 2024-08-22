6112cb1df93d1a878e4a61904f39d7aaeefee1cc
Leozinho11 <leoferraribarreto@hotmail.com>
2024-08-21 22:12:47 -0300

    Fix

--- src/main/java/gp6/harbor/harborapi/domain/pedido/repository/PedidoProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package gp6.harbor.harborapi.domain.pedido.repository;
-
-import gp6.harbor.harborapi.domain.pedido.PedidoProduto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoProdutoRepository extends JpaRepository<PedidoProduto, Integer> {
-}

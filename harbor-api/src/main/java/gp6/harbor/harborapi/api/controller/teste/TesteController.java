92293682eeb52da8a73035ec752658af633a7510
Leozinho11 <leoferraribarreto@hotmail.com>
2024-12-01 23:05:42 -0300

    Create endpoint

---
+++ harbor-api/src/main/java/gp6/harbor/harborapi/api/controller/teste/TesteController.java
@@ -0,0 +1,17 @@
+package gp6.harbor.harborapi.api.controller.teste;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+@RequiredArgsConstructor
+public class TesteController {
+    @GetMapping
+    public ResponseEntity<String> health() {
+        return ResponseEntity.status(200).body("OK");
+    }
+}

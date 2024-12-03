60d6f9607612dec971236d046d36b349a73a1603
Vitor Ramos <vitoramosc@gmail.com>
2024-12-03 10:58:01 -0300

    feat: tratamento de erro ao enviar o email sem o microsservico

--- harbor-api/src/main/java/gp6/harbor/harborapi/service/pedido/PedidoService.java
+++ harbor-api/src/main/java/gp6/harbor/harborapi/service/pedido/PedidoService.java
@@ -23,6 +23,8 @@ import gp6.harbor.harborapi.domain.prestador.repository.PrestadorRepository;
 import gp6.harbor.harborapi.dto.pedido.dto.*;
 import gp6.harbor.harborapi.service.AtividadePedido.AtividadePedidoService;
 import gp6.harbor.harborapi.service.cliente.ClienteService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -61,6 +63,8 @@ public class PedidoService {
     private final PedidoV2Mapper pedidoV2Mapper;
     private final PedidoV2MapperV2 pedidoV2MapperV2;
     private final AtividadePedidoService atividadePedidoService;
+    private static final Logger log = LoggerFactory.getLogger(PedidoService.class);
+
 
     @Transactional
     public PedidoV2 criarPedidoV2(PedidoV2CriacaoDto pedidoDto) {
@@ -174,7 +178,13 @@ public class PedidoService {
         }
         //dar detalhes do pedido no email
         String emailFormatado = emailService.formatarEmail(pedido.getCliente().getEmail(), "AGENDAMENTO REALIZADO COM SUCESSO", pedidoSalvo);
-        emailService.sendEmail(pedido.getCliente().getEmail(), "Agendamento realizado", emailFormatado);
+        try {
+            emailService.sendEmail(pedido.getCliente().getEmail(), "Agendamento realizado", emailFormatado);
+        } catch (Exception e) {
+            // Registrar o erro sem lançar exceção para não interromper o fluxo
+            log.error("Erro ao enviar e-mail para {}: {}", pedido.getCliente().getEmail(), e.getMessage());
+        }
+
 
         AtividadePedido atividadePedido = atividadePedidoService.criarAtividadePedido(pedidoSalvo);
 
